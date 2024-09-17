a03124b3a464f6bd572932b2cf520266d1857c96
tayboyyy <thot62668@gmail.com>
2024-09-17 09:05:45 +0700

    first commit

---
+++ src/tho/dev/data/dao/ProductDao.java
@@ -0,0 +1,13 @@
+package tho.dev.data.dao;
+
+import java.util.List;
+
+import tho.dev.data.model.Product;
+
+public interface ProductDao {
+    public boolean insert(Product product);
+    public boolean update(Product product);
+    public boolean delete(int id);
+    public Product find(int id);
+    public List<Product> findAll();
+}
---
+++ src/tho/dev/data/dao/UserDao.java
@@ -0,0 +1,14 @@
+package tho.dev.data.dao;
+
+import java.util.List;
+
+import tho.dev.data.model.User;
+
+public interface UserDao {
+	public boolean insert(User user);
+	public boolean update(User user);
+	public boolean delete(int userId);
+	public User find(int userId);
+	public List<User> findAll();
+
+}
